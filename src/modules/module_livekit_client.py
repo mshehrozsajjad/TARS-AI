afbe6a90bcd49a0b8fb00c596b2a7168f0929d5e
mshehrozsajjad <mshehrozsajjad@gmail.com>
2026-06-22 17:59:22 +0200

    todo

--- src/modules/module_livekit_client.py
+++ src/modules/module_livekit_client.py
@@ -113,8 +113,8 @@ def _start_display_server(livekit_url, room_name, port=8888):
         _browser_process = subprocess.Popen(
             [
                 chromium_bin,
-                "--kiosk",  # TODO: enable once video display is finalized
-                "--noerrdialogs",
+                # "--kiosk",  # TODO: enable once video display is finalized
+                # "--noerrdialogs",
                 "--disable-infobars",
                 "--disable-session-crashed-bubble",
                 "--autoplay-policy=no-user-gesture-required",
